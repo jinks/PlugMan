b0d50c9645045172796e33faf7dfc228246cd49b
ryanclancy000 <ryanclancy000@gmail.com>
2012-07-13 00:27:31 -0300

    Given option to disable metrics

--- src/main/java/me/ryanclancy000/plugman/PlugMan.java
+++ src/main/java/me/ryanclancy000/plugman/PlugMan.java
@@ -2,37 +2,48 @@ package me.ryanclancy000.plugman;
 
 import java.util.List;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import me.ryanclancy000.plugman.utilities.MetricsLite;
 import me.ryanclancy000.plugman.utilities.Utilities;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlugMan extends JavaPlugin {
 
     public List skipPlugins;
+    private boolean useMetrics;
+    //
     public final Utilities util = new Utilities(this);
-
+    private static final Logger logger = Bukkit.getLogger();
+    
     @Override
     public void onEnable() {
         getCommand("plugman").setExecutor(new PlugManCommands(this));
-        startMetrics();
         loadConfig();
+        startMetrics();
     }
-
-    public void startMetrics() {
+    
+    private void loadConfig() {
         try {
-            MetricsLite metrics = new MetricsLite(this);
-            metrics.start();
+            useMetrics = this.getConfig().getBoolean("use-metrics");
+            skipPlugins = this.getConfig().getList("skip-on-reload");
         } catch (Exception e) {
-            this.getLogger().log(Level.SEVERE, "Failed to load Metrics tracking!{0}", e);
+            logger.log(Level.SEVERE, "Failed to load config - ignoring skip-plugins feature!{0}", e);
+            skipPlugins = null;
         }
     }
 
-    public void loadConfig() {
-        try {
-            skipPlugins = this.getConfig().getList("skip-on-reload");
-        } catch (Exception e) {
-            this.getLogger().log(Level.SEVERE, "Failed to load config - ignoring skip-plugins feature!{0}", e);
-            skipPlugins = null;
+    private void startMetrics() {
+        if (useMetrics) {
+            try {
+                MetricsLite metrics = new MetricsLite(this);
+                metrics.start();
+                logger.log(Level.INFO, "Metrics successfully started!");
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "Failed to start Metrics!{0}", e);
+            }
+        } else {
+            logger.log(Level.INFO, "Ignoring Metrics!");
         }
     }
 }
