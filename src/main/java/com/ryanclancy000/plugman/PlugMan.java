afa83cf38f9a747a97e7229aebca4fcb7785c056
ryanclancy000 <ryanclancy000@gmail.com>
2012-09-30 16:46:42 -0300

    1.7.2

--- src/main/java/com/ryanclancy000/plugman/PlugMan.java
+++ src/main/java/com/ryanclancy000/plugman/PlugMan.java
@@ -4,59 +4,47 @@ import com.ryanclancy000.plugman.utilities.MetricsLite;
 import com.ryanclancy000.plugman.utilities.Utilities;
 import java.util.List;
 import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PlugMan extends JavaPlugin {
+public class PlugMan extends JavaPlugin
+{
+  public List skipPlugins;
+  private transient boolean useMetrics;
+  public final Utilities util = new Utilities(this);
+  public static final Logger logger = Bukkit.getLogger();
 
-    private List<String> skipPlugins;
-    private final Utilities utils = new Utilities(this);
-    
-    @Override
-    public void onDisable() {
-        skipPlugins.clear();
-    }
+  public void onEnable()
+  {
+    getCommand("plugman").setExecutor(new PlugManCommands(this));
+    loadConfig();
+    startMetrics();
+  }
 
-    @Override
-    public void onEnable() {
-        initConfig();
-        initCommands();
-        initMetrics();
+  private void loadConfig() {
+    try {
+      this.useMetrics = getConfig().getBoolean("use-metrics");
+      this.skipPlugins = getConfig().getList("skip-on-reload");
+    } catch (Exception e) {
+      logger.log(Level.SEVERE, "Failed to load config - ignoring skip-plugins feature!{0}", e);
+      this.useMetrics = true;
+      this.skipPlugins = null;
     }
+  }
 
-    private void initConfig() {
-        try {
-            this.getConfig().options().copyDefaults(true);
-            skipPlugins = this.getConfig().getStringList("skip-on-reload");
-            this.saveConfig();
-        } catch (Exception e) {
-            this.getLogger().log(Level.SEVERE, "Failed to load config - ignoring skip-plugins feature!{0}", e);
-            skipPlugins = null;
-        }
-    }
-
-    private void initCommands() {
-        this.getCommand("plugman").setExecutor(new PlugManCommands(this));
-    }
-
-    private void initMetrics() {
-        if (this.getConfig().getBoolean("use-metrics")) {
-            try {
-                MetricsLite metrics = new MetricsLite(this);
-                metrics.start();
-                this.getLogger().log(Level.INFO, "Metrics successfully started!");
-            } catch (Exception e) {
-                this.getLogger().log(Level.SEVERE, "Failed to start Metrics!{0}", e);
-            }
-        } else {
-            this.getLogger().log(Level.INFO, "Ignoring Metrics!");
-        }
-    }
-
-    public List<String> getSkipped() {
-        return skipPlugins;
-    }
-    
-    public Utilities getUtils() {
-        return utils;
-    }
+  private void startMetrics() {
+    if (this.useMetrics)
+      try {
+        MetricsLite metrics = new MetricsLite(this);
+        metrics.start();
+        logger.log(Level.INFO, "Metrics successfully started!");
+      } catch (Exception e) {
+        logger.log(Level.SEVERE, "Failed to start Metrics!{0}", e);
+      }
+    else
+      logger.log(Level.INFO, "Ignoring Metrics!");
+  }
 }
